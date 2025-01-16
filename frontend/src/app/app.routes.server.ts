d6d48057f743c1de84cccad4bae85970d0f4aa72
Muhammad Umer <mumer119131@gmail.com>
2025-01-16 17:56:08 +0500

    added the env prodf

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,8 +1,16 @@
 import { PrerenderFallback, RenderMode, ServerRoute } from '@angular/ssr';
 
+interface PrerenderParams {
+  id: string;
+}
+
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async (): Promise<Record<string, string>[]> => {
+      const id = 'defaultId'; // Replace with appropriate logic to get the id
+      return [{ id }];
+    }
   },
 ];
